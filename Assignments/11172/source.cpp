f19871ab126171d8ce7a01ad1c759c1c6f9d2094
ArshiaClare <35582387+ArshiaClare@users.noreply.github.com>
2020-08-31 11:41:36 -0500

    Create source.cpp

---
+++ Assignments/11172/source.cpp
@@ -0,0 +1,24 @@
+//Arshia Clare
+//11172
+//Aug 31, 2020
+//relational  operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t, a, b;
+
+  cin>>t;
+  
+  for(int i=0; i < t; i++){
+    cin>>a>>b;
+    if(a<b){
+      cout<<"<"<<endl;
+    }else if(a>b){
+      cout<<">"<<endl;
+    }else{
+      cout<<"="<<endl;
+    }
+  }
+}
