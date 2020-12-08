dcf868d22fd4a55e4f0bde9fe48fd1e1f0d624f4
ArshiaClare <35582387+ArshiaClare@users.noreply.github.com>
2020-12-08 00:25:11 -0600

    Update main.cpp

--- Assignments/P04/12347/main.cpp
+++ Assignments/P04/12347/main.cpp
@@ -1,84 +1,69 @@
 #include <iostream>
-
 using namespace std;
 
-struct node 
-{ 
-    int key; 
-    struct node *left, *right; 
-}; 
+struct node {
+   int val;
+   node* left;
+   node* right;
+};
 
-//creates a new node (temp) and return the node
-struct node *newNode(int item) 
-{ 
-    struct node *temp =  (struct node *)malloc(sizeof(struct node)); 
-    temp->key = item; 
-    temp->left = temp->right = NULL; 
-    return temp; 
+node* createNewNode(int x)
+{
+    node* tree = new node;
+    tree->val = x;
+    tree->left  = nullptr;
+    tree->right = nullptr;
+
+    return tree;
 }
 
-//insert the node to BST 
-struct node* insert(struct node* node, int key) 
-{ 
-    /* If the tree is empty, return a new node */
-    if (node == NULL) 
-      return newNode(key); 
-    /* Otherwise, recur down the tree */
-    if (key < node->key) 
-        node->left  = insert(node->left, key); 
-    else if (key > node->key) 
-        node->right = insert(node->right, key);    
-  
-    /* return the (unchanged) node pointer */
-    return node; 
-} 
+void bstInsert(node* &root, int x)
+{
+    if(root == nullptr) {
+        root = createNewNode(x);
+        return;
+    }
 
-//recursive method to find the key
-struct node* search(struct node* root, int key) 
-{ 
-    // Base Cases: root is null or key is present at root 
-    if (root == NULL || root->key == key) 
-       return root; 
-     
-    // Key is greater than root's key 
-    if (root->key < key) 
-       return search(root->right, key); 
-  
-    // Key is smaller than root's key 
-    return search(root->left, key); 
-} 
-   
-// A utility function to do inorder traversal of BST 
-//print method
+    if(x < root->val)
+    {
+        if(root->left == nullptr) {
+            root->left = createNewNode(x);
+            return;
+        } else {
+            bstInsert(root->left, x);
+        }
+    }
+
+    if( x > root->val )
+    {
+        if(root->right == nullptr) {
+            root->right = createNewNode(x);
+            return;
+        } else {
+            bstInsert(root->right, x);
+        }
+    }
+}
+
 void inorder(struct node *root) 
 { 
     if (root != NULL) 
     { 
         inorder(root->left); 
-        printf("%d \n", root->key); 
         inorder(root->right); 
+        cout<<root->val<<endl;
     } 
 } 
 
-int main() 
-{ 
-    /* Let us create following BST 
-              50 
-           /     \ 
-          30      70 
-         /  \    /  \ 
-       20   40  60   80 */
-    struct node *root = NULL; 
-    root = insert(root, 50); 
-    insert(root, 30); 
-    insert(root, 20); 
-    insert(root, 40); 
-    insert(root, 70); 
-    insert(root, 60); 
-    insert(root, 80); 
-   
-    // print inoder traversal of the BST 
-    inorder(root); 
-   
-    return 0; 
-} 
+int main()
+{
+  node* root = nullptr;
+  int num;
+  while(cin>>num){
+    bstInsert(root, num);
+  }
+
+  inorder(root);
+
+  return 0;
+}
